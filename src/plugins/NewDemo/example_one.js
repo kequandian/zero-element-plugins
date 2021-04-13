d987d8d3b5e377b3d59967985c6e2380f6199414
HDJ <263463764@qq.com>
2021-04-13 10:48:56 +0800

    移除 src/utils/request.js

--- src/plugins/NewDemo/example_one.js
+++ src/plugins/NewDemo/example_one.js
@@ -1,27 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Comment from "@/plugins/NewDemo/Comment"
-const promiseAjax = require('@/utils/request');
+import useTokenRequest from 'zero-element-boot/lib/components/hooks/useTokenRequest';
 
 export default function Index(props) {
 
     const api = '/api/adListaaa';
 
-    // const [data, setData] = useState([]);
-
-    // function handleQuery(API, queryData) {
-    //     console.log("API ", API);
-    //     return promiseAjax(API, queryData).then(response => {
-    //         console.log("response ", response);
-    //         if (response && response.code === 200) {
-    //             setData(response.data);
-    //             console.log("response.data ", response.data);
-    //         }
-    //     });
-    // }
-
-    // useEffect(_ => {
-    //     handleQuery(api);
-    // }, []);
+    //const [data]=useTokenRequest(api);
 
     const data = {
         text:'I hope you enjoy learning React!',
--- src/plugins/TodoList/Sandbox.js
+++ src/plugins/TodoList/Sandbox.js
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import TodoList from './index';
 
-// const promiseAjax = require('zero-element-boot/lib/components/utils/request');
-const useTokenRequest = require('zero-element-boot/lib/components/hooks/useTokenRequest');
+import useTokenRequest from 'zero-element-boot/lib/components/hooks/useTokenRequest';
 
 export default function(props){
 
     const api = '/api/TodoList';
-
-    // const [data, setData] = useState([]);
-
-    // function handleQuery(API, queryData) {
-    //     return promiseAjax(API, queryData).then(response => {
-    //         if (response && response.code === 200) {
-    //             setData(response.data);
-    //         }
-    //     });
-    // }
-    // useEffect(_ => {
-    //     handleQuery(api);
-    // }, []);
 
     const [ data ] = useTokenRequest(api);
 
